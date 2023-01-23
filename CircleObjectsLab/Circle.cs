2d5983d480619ad08193f4c6b7449e8fb1fb188c
camdixon132 <camdixon132@gmail.com>
2023-01-23 16:52:26 -0500

    first commit

---
+++ CircleObjectsLab/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace CircleObjectsLab
+{
+	public class Circle
+	{
+		//Properties
+		public double radius { get; set; }
+
+		//Constructors
+		public Circle(double _radius)
+		{
+			radius = _radius;
+		}
+
+		//Methods
+		public double CalculateCircumference()
+		{
+			return 2 * Math.PI * radius;
+		}
+		public string CalculateFormattedCircumference()
+		{
+			return $"Circumference: {FormatNumber(CalculateCircumference())}";
+		}
+		public double CalculateArea()
+		{
+			return radius * radius * Math.PI;
+		}
+		public string CalculateFormattedArea()
+		{
+			return $"Area: {FormatNumber(CalculateArea())}";
+		}
+		private string FormatNumber(double x)
+		{
+			return Math.Round(x,2) + "";
+		}
+
+	}
+}
+
---
+++ CircleObjectsLab/Program.cs
@@ -0,0 +1,32 @@
+﻿using CircleObjectsLab;
+
+bool runProgram = true;
+int circleCount = 0;
+
+
+Console.WriteLine("Welcome to the Circle Tester");
+
+while (runProgram)
+{
+    circleCount += 1;
+
+    Console.WriteLine("Enter radius");
+    double input = double.Parse(Console.ReadLine());
+
+    Circle userCircle = new Circle(input);
+
+    Console.WriteLine(userCircle.CalculateFormattedCircumference());
+    Console.WriteLine(userCircle.CalculateFormattedArea());
+
+    Console.WriteLine("Continue? y/n");
+    string again = Console.ReadLine();
+    if (again == "y" || again == "yes")
+    {
+        runProgram = true;
+    }
+    else
+    {
+        Console.WriteLine($"Goodbye. You created {circleCount} Circle Object(s).");
+        runProgram = false;
+    }
+}
